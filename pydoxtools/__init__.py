346c3f64bdbdf6ca75febe10c6de3d4de8208663
yeus <Thomas.Meschede@web.de>
2022-09-07 00:05:09 +0200

    nice, we can also open html files now :)

--- pydoxtools/__init__.py
+++ pydoxtools/__init__.py
@@ -12,7 +12,7 @@ from pydoxtools import pdf_utils, document
 
 
 class txtExtractor(document.Extractor):
-    def __call__(self, fobj, page_numbers=None, max_pages=0):
+    def __call__(self, fobj, page_numbers=None, max_pages=None):
         with open(fobj) as file:
             txt = file.read()
         return dict(txt=txt)
--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -1,4 +1,5 @@
 import logging
+from io import BytesIO
 
 from pydoxtools import Document
 
@@ -8,16 +9,19 @@ logging.getLogger("pydoxtools.document").setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
 
 pdf_file_name = "../training_data/pdfs/datasheet/BUSI-XCAM-SY-00011.22.pdf"
-#with open(pdf_file_name) as pdf_file:
-#    pdfdoc = Document(fobj=pdf_file)
-#    pdfdoc.x('elements')
+with open(pdf_file_name, "rb") as pdf_file:
+    pdfstr = pdf_file.read()
+
+pdfdoc = Document(fobj=BytesIO(pdfstr))
+pdfdoc.x('elements')
 
 pdfdoc = Document(fobj=pdf_file_name)
 pdfdoc.x('elements')
 
-
-
 with open("../training_data/pdfs/product_page/berrybase_raspberrypi4.html") as html_file:
-    htmldoc = Document(fobj=pdf_file)
+    html_str = html_file.read()
+htmldoc = Document(fobj=html_str)
+
+htmldoc.x('raw_txt')
 
 logger.info("finished!")
