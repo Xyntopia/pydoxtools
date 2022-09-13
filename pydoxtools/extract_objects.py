9eb15b70289e947f8d1f26a32dda91807b7bedbf
yeus <Thomas.Meschede@web.de>
2022-09-13 12:24:54 +0200

    entity extraction using spacy

--- pydoxtools/extract_objects.py
+++ pydoxtools/extract_objects.py
@@ -1,49 +1,61 @@
-@cached_property
-def ner(self) -> List[Tuple[str, str]]:
-    # TODO: add transformers as ner recognition as well:
-    #       from transformers import pipeline
-    #
-    # #ner_pipe = pipeline("ner")
-    # #good results = "xlm-roberta-large-finetuned-conll03-english" # large but good
-    # #name = "sshleifer/tiny-dbmdz-bert-large-cased-finetuned-conll03-english" #small and bad
-    # name = "Davlan/distilbert-base-multilingual-cased-ner-hrl"
-    # model = name
-    # tokenizer= name
-    # ner_pipe = pipeline(task="ner", model=model, tokenizer=tokenizer)
-    # TODO: replace the below by self.model
-    if self._model_size:
-        model_id = nlp_utils.get_spacy_model_id(self.lang, self._model_size)
-        nlp = nlp_utils.load_cached_spacy_model(model_id)
-    else:
-        nlp = nlp_utils.load_cached_spacy_model(self._spacy_model)
+from typing import Any
 
-    res = nlp_utils.extract_entities_spacy(self.full_text, nlp)
-    return res
+import pandas as pd
+
+from pydoxtools import nlp_utils
+from pydoxtools.document import Extractor
 
 
-@cached_property
-def grouped_ner(self) -> Dict[str, Any]:
+class EntityExtractor(Extractor):
+    def __call__(self, spacy_doc) -> dict[str, list[str]]:
+        """TODO: add more entity extraction algorithms (e.g. hugginface)"""
+        # TODO: add transformers as ner recognition as well:
+        #       from transformers import pipeline
+        # #ner_pipe = pipeline("ner")
+        # #good results = "xlm-roberta-large-finetuned-conll03-english" # large but good
+        # #name = "sshleifer/tiny-dbmdz-bert-large-cased-finetuned-conll03-english" #small and bad
+        # name = "Davlan/distilbert-base-multilingual-cased-ner-hrl"
+        # model = name
+        # tokenizer= name
+        # ner_pipe = pipeline(task="ner", model=model, tokenizer=tokenizer)
+
+        entities = []
+        spacy_entities = [(ent.text, ent.label_) for ent in spacy_doc.ents]
+
+        entities.extend(spacy_entities)
+        entities = pd.DataFrame(entities, columns=["entity", "label"])
+        entities.entity = entities.entity.str.strip()
+        # entity_groups = entities.groupby('label').apply(lambda x: x.entity.value_counts().to_dict())
+        entity_groups = entities.groupby('label').entity.apply(list)
+
+        return dict(
+            entities=entity_groups.to_dict()
+        )
+
+
+def grouped_ner(self) -> dict[str, Any]:
     """Group labels from named entity recognition."""
     groups = group_by([n[::-1] for n in self.ner])
     return groups
 
 
-@cached_property
-def urls(self) -> List[str]:
+def urls(self) -> list[str]:
     urls = nlp_utils.get_urls_from_text(self.full_text)
     return urls
 
 
 @property
-def images(self) -> List:
+def images(self) -> list:
     return []
 
-@property
-def titles(self) -> List[str]:
-    return []
 
 @property
-def schemadata(self) -> Dict:
+def titles(self) -> list[str]:
+    return []
+
+
+@property
+def schemadata(self) -> dict:
     """schema.org data extracted from html meta tags and other metainfos from documents
 
     TODO: more detailed description of return type"""
@@ -51,10 +63,11 @@ def schemadata(self) -> Dict:
 
 
 @property
-def product_ids(self) -> Dict[str, str]:
+def product_ids(self) -> dict[str, str]:
     return {}
 
+
 @property
-def price(self) -> List[str]:
+def price(self) -> dict[str]:
     """if prices are given in the document"""
     return []
