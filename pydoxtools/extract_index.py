848c2109b185bf62e5c9a1bfc0441ec2055cbf97
yeus <Thomas.Meschede@web.de>
2022-09-14 09:05:39 +0200

    indexing & query are working again :)

--- pydoxtools/extract_index.py
+++ pydoxtools/extract_index.py
@@ -1,5 +1,10 @@
-import functools
-from document import Extractor
+from typing import Callable
+
+import hnswlib
+import numpy as np
+
+from pydoxtools.document import Extractor, TokenCollection
+
 
 class IndexExtractor(Extractor):
     """
@@ -9,78 +14,74 @@ class IndexExtractor(Extractor):
     --> for example we could also use a scikit-learn knn index or some brute-force method
         etc....
     """
-    @functools.lru_cache()
-    def index(self, filter: str = ""):
+
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, vecs: np.ndarray, ids: list[int]):
         """create a nearest neighbour search index
 
-        pos_filter creates an index only for
+        pos_filter creates an index only for specific word types
 
         TODO: document this function a little better
         """
-        index = hnswlib.Index(space='cosine', dim=self.vectors.shape[1])
+        index = hnswlib.Index(space='cosine', dim=vecs.shape[1])
+        # ids = list(range(len(vecs)))
+        """
         if filter == "noun_chunks":
             vecs = [e.vector for e in self.noun_chunks]
             idx = list(range(len(vecs)))
-
-            index.init_index(max_elements=len(vecs) + 1, ef_construction=200, M=16)
-            # Element insertion (can be called several times):
-            index.add_items(data=vecs, ids=idx)
         elif filter == "sents":
             vecs = [s.vector for s in self.sents]
             idx = list(range(len(vecs)))
-
-            index.init_index(max_elements=len(vecs) + 1, ef_construction=200, M=16)
-            # Element insertion (can be called several times):
-            index.add_items(data=vecs, ids=idx)
         elif filter:
             token_list = [t for t in self.spacy_doc if (t.pos_ in filter)]
             vecs = [t.vector for t in token_list]
             idx = [t.i for t in token_list]
-
-            index.init_index(max_elements=len(vecs) + 1, ef_construction=200, M=16)
-            # Element insertion (can be called several times):
-            index.add_items(data=vecs, ids=idx)
         else:
-            # max_elements defines the maximum number of elements that can be stored in
-            # the structure(can be increased/shrunk).
-            # ef_construction: quality vs speed parameter
-            # M = max number of outgoing connections in the graph...
-            index.init_index(max_elements=len(self.spacy_doc) + 1, ef_construction=200, M=16)
-            # Element insertion (can be called several times):
-            index.add_items(data=self.vectors, ids=list(t.i for t in self.spacy_doc))
-            # Controlling the recall by setting ef:
-            # index.set_ef(100)  # ef should always be > k
+            vecs = self.vecs
+            idx = list(t.i for t in self.spacy_doc)
+        """
+
+        # max_elements defines the maximum number of elements that can be stored in
+        # the structure(can be increased/shrunk).
+        # ef_construction: quality vs speed parameter
+        # M = max number of outgoing connections in the graph...
+        index.init_index(max_elements=len(vecs) + 1, ef_construction=200, M=16)
+        # Element insertion (can be called several times):
+        index.add_items(data=vecs, ids=ids)
+        # Controlling the recall by setting ef:
+        # index.set_ef(100)  # ef should always be > k
 
         return index
 
 
-    def knn_query(
+class KnnQuery(Extractor):
+    def __init__(self):
+        super().__init__()
+        self.interactive()
+
+    def __call__(
             self,
-            txt: Union[str, spacy.tokens.Token, np.ndarray, TokenCollection],
-            k: int = 5, filter="", indices=False
-    ) -> List[Tuple]:
+            index: hnswlib.Index,
+            idx_values: list,
+            vectorizer: Callable,  # e.g. lambda spacy_nlp, txt: spacy_nlp spacy_nlp(txt).vector
+            txt: str | np.ndarray | TokenCollection,
+            k: int = 5, indices=False
+    ) -> list[tuple]:
         if isinstance(txt, str):
-            search_vec = self.spacy_nlp(txt).vector
+            search_vec = vectorizer(txt)
         elif isinstance(txt, np.ndarray):
             search_vec = txt
         else:
             search_vec = txt.vector
-        similar = self.index(filter=filter).knn_query([search_vec], k=k)
-        if filter == "noun_chunks":
-            wordlist = self.noun_chunks
-        elif filter == "sents":
-            wordlist = self.sents
-        else:  # default behaviour
-            wordlist = self.spacy_doc
+        similar = index.knn_query([search_vec], k=k)
 
         if indices:
-            return [(i, wordlist[i], dist) for i, dist in zip(similar[0][0], similar[1][0])]
+            return [(i, idx_values[i], dist) for i, dist in zip(similar[0][0], similar[1][0])]
         else:
-            return [(wordlist[i], dist) for i, dist in zip(similar[0][0], similar[1][0])]
+            return [(idx_values[i], dist) for i, dist in zip(similar[0][0], similar[1][0])]
 
-
-    # @cached_property
-    @functools.lru_cache
     def similarity_graph(self, k=4, max_distance=0.2, method="noun_chunks"):
         """
         this function buils a "directed similarity graph" by taking the similarity of words in a document
