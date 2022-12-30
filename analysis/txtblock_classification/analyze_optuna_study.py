69453b3ec35f65086c5e510b5c0dfe35564728f8
yeus <Thomas.Meschede@web.de>
2022-12-29 23:37:32 -0800

    more & better optuna vizualisation

--- analysis/txtblock_classification/analyze_optuna_study.py
+++ analysis/txtblock_classification/analyze_optuna_study.py
@@ -1,10 +1,12 @@
 from pydoxtools.settings import settings
 import optuna
+import optuna.visualization as ov
 import plotly
 
 optuna.__version__
 
-storage_url = f"sqlite:///{str(settings.MODEL_DIR)}/study.sqlite"
+#storage_url = f"sqlite:///{str(settings.MODEL_DIR)}/study.sqlite"
+storage_url = f"sqlite:////home/tom/Nextcloud/pydoxtools_training/study.sqlite"
 storage_url
 
 study = optuna.load_study(
@@ -15,11 +17,17 @@ study = optuna.load_study(
 
 study.best_params
 
-import optuna.visualization as ov
+ov.plot_contour(study, params=study.best_params.keys()).write_html("/tmp/contour.html")
+
+import subprocess
+subprocess.run(f"google-chrome /tmp/contour.html",shell=True)
+
+# +
+
 # import optuna.visualization.matplotlib as ov
 if ov.is_available():
     figs = {
-        # plot_intermediate_values
+        "intermediate.html": ov.plot_intermediate_values(study),
         # optuna.visualization.plot_pareto_front(study)
         # ov.plot_contour(study, params=study.best_params.keys()).write_html("contour.html")
         "param_importances.html": ov.plot_param_importances(study),
@@ -31,5 +39,6 @@ if ov.is_available():
     for key, fig in figs.items():
         fig.show()
         #fig.write_html()
+# -
 
 
