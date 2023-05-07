0d3d49d3d4360ffd21d7afec6add5e2fc1ce8180
yeus <Thomas.Meschede@web.de>
2023-05-07 15:33:49 -0700

    just a little cleanup

--- examples/quick_sql_query.py
+++ examples/quick_sql_query.py
@@ -6,21 +6,19 @@ an SQL database and inject it into chroma db.
 from pathlib import Path
 
 import dask
-from dask.diagnostics import ProgressBar
 
-import pydoxtools
-from pydoxtools import DocumentBag
+from pydoxtools import DocumentBag, DatabaseSource
 
 dask.config.set(scheduler='synchronous')  # overwrite default with single-threaded scheduler for debugging
 
 # add database information
-database_source = pydoxtools.document.DatabaseSource(
+database_source = DatabaseSource(
     connection_string="sqlite:///" + str(Path.home() / "comcharax/data/component_pages.db"),
     sql="component_pages",  # simply select a table for example
     index_column="id"
 )
 
-# oneliner to extract the information and form an index:
+# Extract the information and form an index:
 # create another DocumentBag using a subset of the extracted tabledata with the key "raw_html"
 # and finally extract a dask-bag of dicts "get_dicts" from the DocumentBag
 # which we will ingest into the vector store.
@@ -29,11 +27,6 @@ docs = DocumentBag(source=database_source).config(doc_configuration=dict(
     vectorizer_only_tokenizer=False,
     vectorizer_model="sentence-transformers/all-MiniLM-L6-v2"
 ))
-d = docs.take(1)[0]
-column = docs.e('data_sel',"url")
-
-with ProgressBar():
-    idx = column.compute_index(100)
-
-# get URLs which are related to "products":
-res = column.query_chroma("product")
+column = docs.e('data_sel', "url")
+idx = column.compute_index(100)
+res = column.query_chroma("product")  # get URLs which are related to "products":
--- pydoxtools/__init__.py
+++ pydoxtools/__init__.py
@@ -2,7 +2,7 @@ __version__ = '0.5.1'
 
 import logging
 
-from .document import Document, DocumentBag
+from .document import Document, DocumentBag, DatabaseSource
 from .document_base import Pipeline
 
 logger = logging.getLogger(__name__)
