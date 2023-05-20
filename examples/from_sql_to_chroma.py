124139e39da565c49332f9596c544ebfb6c6ef4d
yeus <Thomas.Meschede@web.de>
2023-05-20 12:28:23 -0700

    dask distributed ingestion into vectordb works ...

--- examples/from_sql_to_chroma.py
+++ examples/from_sql_to_chroma.py
@@ -4,60 +4,94 @@ an SQL database and inject it into chroma db.
 """
 
 import logging
+import uuid
 from pathlib import Path
 
 import chromadb
-import dask
+from chromadb.config import Settings
 from dask.diagnostics import ProgressBar
 
 import pydoxtools
-from pydoxtools import DocumentBag, Document
+from pydoxtools import DocumentBag
+from pydoxtools.settings import settings
 
 logger = logging.getLogger(__name__)
 
-dask.config.set(scheduler='synchronous')  # overwrite default with single-threaded scheduler for debugging
+if __name__ == "__main__":
+    # in order to "scale up" the operation, we can use dask distributed.
+    # As pydoxtools.Document is pickable, this is possible..
+    import dask
+    dask.config.set(scheduler='synchronous')  # overwrite default with single-threaded scheduler for debugging
+    # need to be inside the "__main__".
+    # from dask.distributed import Client
+    #client = Client()  # set up local cluster on your laptop
 
-chroma_client = chromadb.Client()
-collection = chroma_client.get_or_create_collection(name="index")
+    # add database information
+    # connecting to postgres could be done like this:
+    # 'postgresql+psycopg2://user:password@hostname/db_name'
+    # it would also require installing the postgresql driver: pip install psycopg2
+    database_source = pydoxtools.document.DatabaseSource(
+        connection_string="sqlite:///" + str(Path.home() / "comcharax/data/component_pages.db"),
+        sql="component_pages",  # select a table or use an actual SQL string
+        index_column="id"
+    )
 
-# add database information
-# connecting to postgres could be done like this:
-# 'postgresql+psycopg2://user:password@hostname/db_name'
-# it would also require installing the postgresql driver: pip install psycopg2
-database_source = pydoxtools.document.DatabaseSource(
-    connection_string="sqlite:///" + str(Path.home() / "comcharax/data/component_pages.db"),
-    sql="component_pages",  # select a table or use an actual SQL string
-    index_column="id"
-)
+    # Extract the information and form an index:
+    # create another DocumentBag using a subset of the extracted tabledata with the key "raw_html"
+    # and finally extract a dask-bag of dicts "get_dicts" from the DocumentBag
+    # which we will ingest into the vector store.
+    # or as a one-liner:
+    #   DocumentBag(source=database_source).get_data_docbag(column).get_dicts("source", "full_text", "vector")
+    # this will load each table row into a single row as a "dictionary"
+    table = DocumentBag(source=database_source).config(doc_configuration=dict(
+        # here we can choose to do some fast vectorization by usong only the tokenizer
+        vectorizer_only_tokenizer=True,
+        vectorizer_model="sentence-transformers/all-MiniLM-L6-v2"
+    ))
 
-# Extract the information and form an index:
-# create another DocumentBag using a subset of the extracted tabledata with the key "raw_html"
-# and finally extract a dask-bag of dicts "get_dicts" from the DocumentBag
-# which we will ingest into the vector store.
-# or as a one-liner:
-#   DocumentBag(source=database_source).get_data_docbag(column).get_dicts("source", "full_text", "vector")
-# this will load each table row into a single row as a "dictionary"
-table = DocumentBag(source=database_source).config(doc_configuration=dict(
-    # here we can choose to do some fast vectorization by usong only the tokenizer
-    vectorizer_only_tokenizer=True,
-    vectorizer_model="sentence-transformers/all-MiniLM-L6-v2"
-))
+    # In this apply operation we choose the information that we want from
+    # each row in the table (represented by a pydoxtools.Document).
+    # Document.apply automatically create a new list documents of it and
+    # returns that as a new DocumentBag
+    db_idx = table.apply(new_document=lambda d: [d.data["url"], d.data["scrape_time"]])
 
-compute, query = table.apply(
-    new_document=lambda d: [d.data["url"], d.data["scrape_time"], ]
-).add_to_chroma(
-    collection,
-    # embeddings="embedding", # we can customize which embeddings to take here, default is "embedding" of the full text
-    # document="full_text", # we can customize which field to take here, default is "full_text"
-    metadata="source",  # this is also customizable, the default would simply take the "metadata" of each document
-    ids=None  # we can also create ids from the document for example using the "source" as id or similar
-)
+    # we can now add the contents of our index-DocumentBagto chromadb
+    def insert(docs):
+        chroma_client = chromadb.Client(Settings(
+            chroma_db_impl="duckdb+parquet",
+            persist_directory=str(settings.PDX_CACHE_DIR_BASE / "chromadb")
+        ))
 
-with ProgressBar():
-    compute(20)  # choose a number how many rows you would like to add ro your chromadb!
+        collection = chroma_client.get_or_create_collection(name="index")
 
-res = query(embeddings=Document("list").embedding.tolist())
-res = query("raspberry pi products")
+        for i,doc in enumerate(docs):
+            collection.add(
+                embeddings=[[float(n) for n in doc.embedding]],
+                documents=[doc.full_text],
+                metadatas=[{"source": str(doc.source)}],
+                ids=[uuid.uuid4().hex]
+            )
 
-# query the db:
-# column.query_chroma("product")
+        chroma_client.persist()
+
+        return range(i)
+
+
+    #with ProgressBar():
+    a = db_idx.docs.map_partitions(insert).take(100)
+        # compute(20)  # choose a number how many rows you would like to add ro your chromadb!
+
+    """
+    compute, query = db_idx.add_to_chroma(
+        collection,
+        # embeddings="embedding", # we can customize which embeddings to take here, default is "embedding" of the full text
+        # document="full_text", # we can customize which field to take here, default is "full_text"
+        metadata="source",  # this is also customizable, the default would simply take the "metadata" of each document
+        ids=None  # we can also create ids from the document for example using the "source" as id or similar
+    )
+    """
+
+    #res = query("raspberry pi products")
+
+    # query the db:
+    # column.query_chroma("product")
