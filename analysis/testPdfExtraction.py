be6af2b2eea6757a62b7376991c905a6533f3c97
yeus <Thomas.Meschede@web.de>
2023-11-01 12:26:43 -0700

    reading xref is sort-of working...

--- analysis/testPdfExtraction.py
+++ analysis/testPdfExtraction.py
@@ -1,19 +1,128 @@
 def parse_pdf_header(pdf_content):
-    # The PDF header is usually within the first 1024 bytes
+    # The PDF header starts with '%PDF-' and is usually within the first few lines
     header_position = pdf_content.find(b'%PDF-')
     if header_position == -1:
         raise ValueError("Not a valid PDF file")
-    header_end = pdf_content.find(b'\r\n', header_position)
-    return pdf_content[header_position:header_end].decode()
+
+    # Look for the first newline character to find the end of the header
+    # Newline characters can be either '\n', '\r\n', or '\r'
+    header_end = pdf_content.find(b'\n', header_position)
+    if header_end == -1:
+        header_end = pdf_content.find(b'\r\n', header_position)
+    if header_end == -1:
+        header_end = pdf_content.find(b'\r', header_position)
+
+    # If no newline character is found, the header might be followed by a comment or object definition
+    # In this case, limit the header to the first 20 bytes after '%PDF-'
+    if header_end == -1:
+        header_end = header_position + 20
+
+    # Decode with 'ignore' to avoid UnicodeDecodeError
+    return pdf_content[header_position:header_end].decode('utf-8', 'ignore')
+
 
 def find_xref_position(pdf_content):
     # The xref position is typically near the end of the file
     xref_position = pdf_content.rfind(b'startxref')
     if xref_position == -1:
         raise ValueError("xref not found")
-    xref_position_start = pdf_content.find(b'\r\n', xref_position) + 2
-    xref_position_end = pdf_content.find(b'\r\n', xref_position_start)
-    return int(pdf_content[xref_position_start:xref_position_end])
+
+    # Skip the 'startxref' keyword and any subsequent whitespace
+    xref_position_start = xref_position + len(b'startxref')
+    while pdf_content[xref_position_start] in b'\x20\x0A\x0D\x09':  # Space, LF, CR, Tab
+        xref_position_start += 1
+
+    # Find the end of the xref position number
+    xref_position_end = xref_position_start
+    while pdf_content[xref_position_end] not in b'\x20\x0A\x0D\x09':  # Space, LF, CR, Tab
+        xref_position_end += 1
+
+    # Convert the xref position to an integer
+    xref_position_number = int(pdf_content[xref_position_start:xref_position_end])
+
+    return xref_position_number
+
+
+def get_object(pdf_content, obj_num, xref_table):
+    # This is a simplified example and does not handle all object types or errors
+    obj_pos = xref_table[obj_num]
+    obj_header = pdf_content[obj_pos:].split(b'\n')[0]
+    if b'stream' in obj_header:
+        # Handle stream object
+        pass  # ...
+    else:
+        # Handle other object types
+        pass  # ...
+
+
+def get_pages(catalog):
+    # Assuming catalog is a dictionary with a 'Pages' key
+    return catalog.get('Pages', [])
+
+
+def get_stream_data(xobject):
+    # Assuming xobject is a dictionary with a 'Stream' key
+    return xobject.get('Stream', b'')
+
+
+def extract_base_elements(pdf_content, xref_position):
+    # This is a simplified example and does not handle all cases or errors
+    xref_table = parse_xref_table(pdf_content, xref_position)
+    catalog = get_object(pdf_content, 1, xref_table)  # Assume object 1 is the catalog
+    pages = get_pages(catalog)  # You'll need to write this function
+    for page in pages:
+        resources = page['Resources']
+        fonts = resources['Font']
+        for font in fonts.values():
+            # Do something with font information
+            pass
+        xobjects = resources.get('XObject', {})
+        for xobject in xobjects.values():
+            # Check if the XObject is an image
+            if xobject['Subtype'] == 'Image':
+                image_data = get_stream_data(xobject)  # You'll need to write this function
+                # Do something with image data
+                pass
+            elif xobject['Subtype'] == 'Form':
+                # Recurse into form XObjects
+                extract_base_elements(xobject['Resources'])
+
+
+def parse_xref_table(pdf_content, xref_position):
+    pos = xref_position
+    xref_table = {}
+
+    # Skip the 'xref' line and the object range line
+    pos = pdf_content.find(b'\n', pos) + 1  # Skip 'xref'
+    pos = pdf_content.find(b'\n', pos) + 1  # Skip object range
+
+    while True:
+        # Get the next line
+        newline_pos = pdf_content.find(b'\n', pos)
+        if newline_pos == -1:
+            raise ValueError("Trailer not found")
+
+        line = pdf_content[pos:newline_pos].decode('utf-8', 'ignore')
+
+        if line.startswith('trailer'):
+            # End of xref table
+            break
+
+        # Parse the fields based on their fixed widths
+        obj_offset = int(line[:10])
+        obj_gen_num = int(line[11:16])
+        obj_status = line[17]
+
+        # Assuming you want to store the offset of in-use objects
+        if obj_status == 'n':
+            obj_num = len(xref_table)  # Assuming objects are listed in order
+            xref_table[obj_num] = obj_offset
+
+        # Move to the next line
+        pos = newline_pos + 1
+
+    return xref_table
+
 
 with open('../tests/data/Datasheet-Centaur-Charger-DE.6f.pdf', 'rb') as file:
     pdf_content = file.read()
@@ -21,5 +130,8 @@ with open('../tests/data/Datasheet-Centaur-Charger-DE.6f.pdf', 'rb') as file:
 header = parse_pdf_header(pdf_content)
 xref_position = find_xref_position(pdf_content)
 
-print(f"PDF Header: {header}")
-print(f"xref Position: {xref_position}")
+print(f"PDF Header: {header}\n\n")
+print(f"xref Position: {xref_position}\n\n")
+print(pdf_content[xref_position:xref_position + 100].decode('utf-8', 'ignore'))
+
+xref_table = parse_xref_table(pdf_content, xref_position)
