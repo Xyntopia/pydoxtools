045014f10fb74e02d8a0e6246d9858b83814db12
yeus <Thomas.Meschede@web.de>
2023-11-04 18:39:02 -0700

    can now parse most options....

--- analysis/testPdfExtraction.py
+++ analysis/testPdfExtraction.py
@@ -168,36 +168,30 @@ class Visitor(NodeVisitor):
         return float(node.text)
 
     def visit_entry(self, node, visited_children):
-        _, key, _, val, _ = visited_children
+        key, val = visited_children
         return (key, val)
 
     def visit_value(self, node, visited_children):
-        return visited_children[0]
+        return visited_children[2][0]
 
     def visit_string(self, node, visited_children):
         return visited_children[1]
-
-    def visit_uristr(self, node, visited_children):
-        return visited_children[1].text
 
     def visit_date(self, node, visited_children):
         return visited_children[1]
 
     def visit_reference(self, node, visited_children):
         int1, _, int2, _, _ = visited_children
-        return ("R", int1, int2)
+        return (int1, int2, "R")
 
     def visit_list(self, node, visited_children):
         newlist = visited_children[1]
         return newlist
 
-    def visit_strlist(self, node, visited_children):
-        newlist = visited_children[1]
-        return newlist
-
     def visit_array(self, node, visited_children):
+        array = visited_children[1]
         # whitespace is usually "None" thats why we're checking...
-        array = tuple(v[0] for v in visited_children if (v[0] is not None))
+        array = tuple(v[0][0] for v in array if (v[0] is not None))
         return array
 
     def visit_dict(self, node, visited_children):
@@ -205,22 +199,14 @@ class Visitor(NodeVisitor):
         return out
 
     def visit_text(self, node, visited_children):
-        return node.text
+        return node.text.strip()
 
     def visit_word(self, node, visited_childre):
         return node.text
 
     def visit_key(self, node, visited_children):
-        _, word = visited_children
+        word = visited_children[1].text
         return word
-
-    def visit_font(self, node, visited_children):
-        prefix, flag, fontname = visited_children
-        font = {"prefix": prefix, "fontname": flag.text + fontname}
-        return font
-
-    def visit_fontname(self, node, visited_children):
-        return node.text
 
     def generic_visit(self, node, visited_children):
         """ The generic visit method. """
@@ -228,34 +214,24 @@ class Visitor(NodeVisitor):
 
 
 pdf_obj_grammar = Grammar(r"""
-    pdfobj = obj_id objcontent "endobj"
+    pdfobj = obj_id objcontent endobj
     objcontent = objentry*
     objentry = props / int / ws
     props = "<<" dict ">>"
     dict = entry*
-    entry = ws? key ws? value ws?
-    value = date  / reference / int / props / font / list / key / text / uristr / string / strlist
-    list = "[" array "]"
-    strlist = "(" array ")"
-    uristr = "(" uri ")"
-    string = "(" text ")"
-    array = (reference / key / number / int / word / ws)+
-    key = "/" word
+    entry = key value
+    value = ws? "/"? (reference / int / text / list / props / word) ws?
+    endobj = "endobj"
+    list = ("[" / "(") array ("]" / ")")
+    array = ws? ((reference / key / number / int / word) ws?){1,} ws?
+    key = "/" ~"[a-z0-9]+"is
     obj_id = int ws int ws "obj"
     reference = int ws int ws "R"
-    # date format:  (D:YYYYMMDDHHmmSSOHH'mm') 
-    # O is the relationship of local time to Universal Time (UT), denoted by one of the characters +, -, or Z (see below)
-    # HH followed by ' is the absolute value of the offset from UT in hours (00-23)
-    # mm followed by ' is the absolute value of the offset from UT in minutes (00-59)
-    date = "(D:" text ")"
-    uri = ~"(?:mailto:)?[\w.-]+@[\w.-]+\.\w+"i
-    font = key "+" fontname
-    fontname = word ("-" word)*
-    word = ~"[a-z0-9]+"i
-    text = ~"[ a-z0-9.-]+"i
+    text = (word / ws)+
+    word = ~"[a-z0-9.\-:@\+]+"i
     int = ~"-?[0-9]+"
-    number = ~"-?[0-9.]+"
-    ws = ~"\s*"
+    number = ~"-?[0-9]+\.[0-9]+"
+    ws = ~"\s+"
 """)
 
 iv = Visitor()
@@ -313,6 +289,14 @@ if True:
 
         # def test_object_extraction():
         texts = (
+            b'3 0 obj\n<< /Type /Pages /Kids [\n4 0 R\n21 0 R\n] /Count 2\n/Rotate 0>>\nendobj'
+,
+            b'11 0 obj\n<</BaseFont/XRQUZW+MyriadPro-Regular/FontDescriptor 10 0 R/Type/Font\n/FirstChar 1/LastChar 86/Widths[ 212 736 207 481 234\n448 331 327 549 555 ]\n/Encoding 44 0 R/Subtype/Type1>>\nendobj'
+            ,
+            b'11 0 obj\n<</BaseFont/XRQUZW+MyriadPro-Regular/FontDescriptor 10 0 R/Type/Font\n/FirstChar 1/LastChar 86/Widths[ 212 736 207 481 234\n448 331 327 549 555 ]\n/Encoding 44 0 R/Subtype/Type1>>\nendobj'
+            ,
+            b'17 0 obj\r\n<</Type/Font/Subtype/Type0/BaseFont/ArialMT/Encoding/Identity-H/DescendantFonts 18 0 R/ToUnicode 209 0 R>>\r\nendobj'
+            ,
             b'13 0 obj\r\n<</Subtype/Link/Rect[ 307.54 52.669 404.42 67.927] /BS<</W 0>>/F 4/A<</Type/Action/S/URI/URI(mailto:info@berlin-space-tech.com) >>>>\r\nendobj'
             ,
             b'3 0 obj\r\n<</Type/Page/Parent 2 0 R/Resources<</Font<</F1 5 0 R/F2 9 0 R/F3 11 0 R/F4 15 0 R/F5 17 0 R/F6 22 0 R>>/ExtGState<</GS7 7 0 R/GS8 8 0 R>>/XObject<</Image14 14 0 R/Image24 24 0 R>>/ProcSet[/PDF/Text/ImageB/ImageC/ImageI] >>/Annots[ 13 0 R] /MediaBox[ 0 0 595.32 841.92] /Contents 4 0 R/Group<</Type/Group/S/Transparency/CS/DeviceRGB>>/Tabs/S/StructParents 0>>\r\nendobj'
@@ -334,4 +318,4 @@ if True:
 
         # test_object_extraction()
 
-        object_props = {num: parse_pdf_object(value) for num, value in object_dict.items()}
+        # = {num: parse_pdf_object(value) for num, value in object_dict.items()}
